3eeaf939a925465e48200379e03323247a706a5f
RJ Salvador <rjsalvadorr@gmail.com>
2019-09-10 12:19:15 -0400

    Customized typefaces

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,23 +1,37 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from 'typography';
+import Wordpress2016 from 'typography-theme-wordpress-2016';
+
+Wordpress2016.googleFonts = [
+  {
+    name: 'Open Sans',
+    styles: ['400'],
+  },
+  {
+    name: 'Special Elite',
+    styles: ['400'],
+  },
+];
+Wordpress2016.headerFontFamily = ['Special Elite', 'sans-serif'];
+Wordpress2016.bodyFontFamily = ['Open Sans', 'sans-serif'];
 
 Wordpress2016.overrideThemeStyles = () => {
   return {
-    "a.gatsby-resp-image-link": {
+    'h1, h2, h3, h4, h5, h6': {
+      fontFamily: ['Special Elite', 'sans-serif'].join(','),
+    },
+    'a.gatsby-resp-image-link': {
       boxShadow: `none`,
     },
-  }
-}
+  };
+};
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography (Wordpress2016);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
+  typography.injectStyles ();
 }
 
-export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
+export default typography;
+export const rhythm = typography.rhythm;
+export const scale = typography.scale;
