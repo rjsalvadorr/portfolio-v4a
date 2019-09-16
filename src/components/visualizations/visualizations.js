2dc7b7c47c17b181e25a1a2e4196bd09c373f869
RJ Salvador <rjsalvadorr@gmail.com>
2019-09-15 20:08:58 -0400

    #29 - Homepage visuals switchin up (#30)
    * #29 - homepage visuals now switch automatically
    
    * #29 - fixins

--- src/components/visualizations/visualizations.js
+++ src/components/visualizations/visualizations.js
@@ -1,45 +1,88 @@
 import React from 'react';
-import threeUtils from './utils/three-utils';
 import ThreeCity from './three-city';
 import DynamicGrid from './dynamic-grid';
 import BrokenGrid from './broken-grid';
-import vizStyles from "../../styles/visualizations.module.css"
+import vizStyles from '../../styles/visualizations.module.css';
 
 class Visualizations extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      overlayEnabled: true,
+      overlayOff: false,
+      currentVisual: 0,
     };
     this.visualsRef = React.createRef ();
     this.fadeIn = this.fadeIn.bind (this);
+    this.fadeOut = this.fadeOut.bind (this);
+    this.resetVisual = this.resetVisual.bind (this);
+    this.intervalId = null;
+    this.intervalId2 = null;
   }
 
   fadeIn () {
-    this.setState ({overlayEnabled: true});
+    this.setState ({overlayOff: true});
+  }
+
+  fadeOut () {
+    this.setState ({overlayOff: false});
+  }
+
+  resetVisual () {
+    let nextVis = this.state.currentVisual + 1;
+    if (nextVis > 3) {
+      nextVis = 1;
+    }
+    this.setState ({
+      overlayOff: true,
+      currentVisual: nextVis,
+    });
+  }
+
+  componentDidMount () {
+    const reset = this.resetVisual;
+    const fade = this.fadeOut;
+
+    const time = 5000; // five seconds
+
+    this.intervalId = window.setInterval (function () {
+      reset ();
+      setTimeout (function () {
+        fade ();
+      }, time - 500);
+    }, time);
+
+    reset ();
+    setTimeout (function () {
+      fade ();
+    }, time - 500);
   }
 
   render () {
-    let visual = (<ThreeCity />);
+    let visual = <ThreeCity />;
+    let overlayClass = this.state.overlayOff
+      ? `${vizStyles.vizOverlay} ${vizStyles.vizOverlayOff}`
+      : `${vizStyles.vizOverlay}`;
 
-    const randomVis = threeUtils.getRandomInt (1, 3);
-    switch (randomVis) {
+    switch (this.state.currentVisual) {
       case 2:
-        visual = (<DynamicGrid />);
+        visual = <DynamicGrid />;
         break;
       case 3:
-        visual = (<BrokenGrid />);
+        visual = <BrokenGrid />;
         break;
       default:
-        visual = (<ThreeCity />);
+        visual = <ThreeCity />;
     }
 
     return (
-      <div
-        className={`${vizStyles.vizWrapper} visuals--${randomVis}`}
-        ref={this.visualsRef}
-      >
-        {visual}
+      <div>
+        <div className={overlayClass} />
+        <div
+          className={`${vizStyles.vizWrapper} visuals--${this.state.currentVisual}`}
+          ref={this.visualsRef}
+        >
+          {visual}
+        </div>
       </div>
     );
   }
